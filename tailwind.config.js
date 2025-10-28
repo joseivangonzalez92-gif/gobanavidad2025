7fae4ba4b7a4277dbd5c6ffb757abf3ba0f7ae90
joseivangonzalez92-gif <joseivan.gonzalez92@gmail.com>
2025-10-28 02:19:02 -0600

    Remove unused paths from Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,7 @@
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-    "./public/**/*.html",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}", 
   ],
   theme: {
     extend: {},
