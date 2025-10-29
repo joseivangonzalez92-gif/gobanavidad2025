f4e7133f91ae243a2555919bc7296dd1445d0041
joseivangonzalez92-gif <joseivan.gonzalez92@gmail.com>
2025-10-29 09:14:07 -0600

    fix: production build working

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist'
+    outDir: 'dist',
   },
-  // Ayuda con el routing en producción
-  server: {
-    host: true
-  }
+  base: './'
 })
