84d45e36cc6cfea6642a3f2baaa6223e49a04c36
joseivangonzalez92-gif <joseivan.gonzalez92@gmail.com>
2025-10-29 10:22:57 -0600

    fix: update vite config to properly resolve jsx files

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist',
-    rollupOptions: {
-      input: './index.html'
-    }
-  },
-  // 👇 AGREGAR ESTO
-  optimizeDeps: {
-    include: ['react', 'react-dom', 'react-router-dom']
+    outDir: 'dist'
   }
 })
